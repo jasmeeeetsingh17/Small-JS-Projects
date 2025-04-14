97be481f2880d9a2b50b4b7fe62d37663d1a4f35
Jasmeet Singh <jasmeetsingh1710@gmail.com>
2025-04-14 16:58:57 +0530

    Add Digital Clock and Rock Paper Scissor projects

---
+++ Rock_Paper_scissor/app.js
@@ -0,0 +1,49 @@
+let score = 0;
+let computer_score = 0
+let img = document.querySelectorAll('img');
+let choices = ['rock', 'paper', 'scissors'];
+let userSelection;
+let user_score = document.querySelector('#score');
+let computer_scores = document.querySelector('#computer-score');
+
+img.forEach((img) => {
+    img.addEventListener('click', () => {
+        userSelection = img.id;
+        check(userSelection);
+        console.log(userSelection);
+    });
+});
+
+const check = (userSelection) => {
+    const computerChoices = () => {
+        const computer_choose = Math.floor(Math.random() * choices.length);
+        return choices[computer_choose];
+    }
+
+    computer_choice = computerChoices();
+    console.log(computer_choice);
+
+    if (userSelection === computer_choice) {
+        let newElement = document.querySelector('#hint');
+        newElement.innerText = "TIE";
+    }
+    else if (
+        (userSelection === "rock" && computer_choice === "scissors") ||
+        (userSelection === "paper" && computer_choice === "rock") ||
+        (userSelection === "scissors" && computer_choice === "paper")) {
+        let newElement = document.querySelector('#hint');
+        newElement.innerText = "You Win !!";
+        score += 1;
+    }
+    else {
+        let newElement = document.querySelector('#hint');
+        newElement.innerText = "You Lose !!";
+        computer_score += 1;
+    }
+
+    user_score.innerText = `Your Score is ${score}`;
+    computer_scores.innerText = `Computer Score is ${computer_score}`;
+
+};
+
+
